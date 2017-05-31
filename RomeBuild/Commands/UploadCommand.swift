8de09d6300b11937dc56a575bcd0b95525bd573a
Luiz Dubas <luizdubas@gmail.com>
2017-05-31 13:44:39 -0300

    Improve Upload Command

--- RomeBuild/Commands/UploadCommand.swift
+++ RomeBuild/Commands/UploadCommand.swift
@@ -7,8 +7,10 @@ struct UploadCommand {
         
         if !Cartfile().exists() {
             Carthage(["update", "--no-build"])
+            Carthage(["checkout", "--no-use-binaries"])
         } else {
             Carthage(["checkout"])
+            Carthage(["checkout", "--no-use-binaries"])
         }
         
         var dependenciesToBuild = [String:String]()
@@ -23,15 +25,30 @@ struct UploadCommand {
         }
         
         let dependenciesToUploadArray = Array(dependenciesToBuild.keys.map { $0 })
+        let directory = Environment().currentDirectory()!
+        let buildDir = "Carthage/Build/"
+        print("Current dir \(directory)")
+        
+        try! NSFileManager.defaultManager().createDirectoryAtPath(buildDir,
+                                                                  withIntermediateDirectories: true,
+                                                                  attributes: nil)
         
         if dependenciesToUploadArray.count > 0 {
             for dependency in dependenciesToUploadArray {
-                let dependencyPath = "\(Environment().currentDirectory()!)/Carthage/Checkouts/\(dependency)"
+                let dependencyPath = "\(directory)/Carthage/Checkouts/\(dependency)"
                 let carthageBuildPath = "Carthage/Checkouts/\(dependency)"
+                let carthageDepBuildPath = "\(carthageBuildPath)/\(buildDir)"
+                
+                
+                try? NSFileManager.defaultManager().removeItemAtPath(carthageDepBuildPath)
+                
+                try! NSFileManager.defaultManager().copyItemAtPath(buildDir,
+                                                                   toPath: carthageDepBuildPath)
                 
                 print("Trying to pre archive \(dependency)")
                 var status = Carthage(["archive", "--project-directory", carthageBuildPath])
                 if status.status != 0 {
+                    try? NSFileManager.defaultManager().removeItemAtPath(carthageDepBuildPath)
                     print("Couldn't find framework\nCheckout project dependency \(dependency)")
                     Carthage(["checkout", dependency])
                     
@@ -48,7 +65,7 @@ struct UploadCommand {
                     }
                     
                     Carthage(buildArchive)
-                    status = Carthage(["archive", "--output", Environment().currentDirectory()!], path: dependencyPath)
+                    status = Carthage(["archive", "--output", directory], path: dependencyPath)
                 }
                 Helpers().uploadAsset(dependency, revision: dependenciesToBuild[dependency]!, filePath: getFrameworkPath(status))
             }
